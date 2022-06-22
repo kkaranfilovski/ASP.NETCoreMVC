e187254820f9b66d6fa5d143e12130f9469b19b1
Kristijan <kar.kristijan@gmail.com>
2022-06-22 20:52:55 +0200

    init

---
+++ Class04/SEDC.Class04/SEDC.Class04.DEMO/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.Class04.DEMO.Models;
+
+namespace SEDC.Class04.DEMO.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            var user = new User
+            {
+                Id = 1,
+                FirstName = "Kristijan",
+                LastName = "Karanfilovski",
+                Age = 27,
+                City = "Skopje",
+                IsEmployed = true,
+            };
+            return View(user);
+        }
+    }
+}
---
+++ Class04/SEDC.Class04/SEDC.Class04.DEMO/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace SEDC.Class04.DEMO.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public bool IsEmployed { get; set; }
+
+        public override string ToString()
+        {
+            return $"Hello there! I am {FirstName} {LastName}";
+        }
+    }
+}
