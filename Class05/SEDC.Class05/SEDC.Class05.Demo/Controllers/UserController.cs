8850b8ef1966364ed4dfbadc61286cfe05c5d619
Kristijan <kar.kristijan@gmail.com>
2022-06-25 12:20:23 +0200

    init

---
+++ Class05/SEDC.Class05/SEDC.Class05.Demo/Controllers/UserController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.Class05.Demo.Models;
+
+namespace SEDC.Class05.Demo.Controllers
+{
+    public class UserController : Controller
+    {
+        [Route("user")]
+        public IActionResult Index()
+        {
+            var user = StaticDb.Users.SingleOrDefault(x => x.Id == 1);
+            var u = new User();
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var user = new User();
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (user != null)
+                {
+                    user.Id = StaticDb.Users.Count + 1;
+                    StaticDb.Users.Add(user);
+                    return RedirectToAction("Users");
+                }
+            }
+            return View();
+        }
+
+        [HttpGet("all")]
+        public IActionResult Users()
+        {
+            var users = StaticDb.Users;
+            return View(users);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var user = StaticDb.Users.SingleOrDefault(x => x.Id == id);
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(User user)
+        {
+            if (user != null)
+            {
+                var userFromDb = StaticDb.Users.SingleOrDefault(x => x.Id == user.Id);
+                userFromDb.FirstName = user.FirstName;
+                userFromDb.LastName = user.LastName;
+                userFromDb.Age = user.Age;
+                userFromDb.City = user.City;
+                return RedirectToAction("Users");
+            }
+            ViewBag.ErrorMessage = "User update failed! Contact support!";
+            return View();
+        }
+    }
+}
---
+++ Class05/SEDC.Class05/SEDC.Class05.Demo/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SEDC.Class05.Demo.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Display(Name = "First name")]
+        [Required(ErrorMessage = "Please fill this field")]
+        //[MinLength(3, ErrorMessage = "Enter more then 3 characters")]
+        [StringLength(50, ErrorMessage ="From 2 to 50 char", MinimumLength = 2)]
+        public string FirstName { get; set; }
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+        [Display(Name = "Age")]
+        public int Age { get; set; }
+        [Display(Name = "City of residence")]
+        public string City { get; set; }
+        //public bool IsEmployed { get; set; }
+
+        public override string ToString()
+        {
+            return $"Hello there! I am {FirstName} {LastName}";
+        }
+    }
+}
